78a96b684080b2a9df3a4cb95721aaad923987f4
Gram Liu <gram.liu226@gmail.com>
2022-10-16 23:06:17 -0400

    Setup script execution with params

--- o1.py
+++ o1.py
@@ -1,16 +1,51 @@
 import argparse
+import subprocess
 from src.get_commands import get_commands
 
+# Configure root and subparsers
 parser = argparse.ArgumentParser(description="Run predefined commands")
-parser.add_argument("-l", "--list", action="store_true",
-                    help="List available commands")
+subparsers = parser.add_subparsers(dest="action", description="Action to run")
+list_parser = subparsers.add_parser(
+    "list", description="List available commands")
+run_parser = subparsers.add_parser("run", description="Run a command")
 
-args = parser.parse_args()
-if args.list:
-    commands = get_commands()
+# Load command parsers
+commands = get_commands()
+run_subparsers = run_parser.add_subparsers(
+    dest="command", description="Command to run")
+for command_name in commands.keys():
+    command = commands[command_name]
+
+    examples = "\n".join(
+        map(lambda example: f"o1 run {example}", command.examples))
+    cmd_parser = run_subparsers.add_parser(
+        command_name, description=f"{command.description}", usage=examples)
+
+    cli_args = command.get_arguments()
+    for arg in cli_args:
+        cmd_parser.add_argument(
+            f"--{arg.name}", f"-{arg.alias}", help=arg.description, required=arg.required)
+
+cli_args = parser.parse_args()
+
+if cli_args.action == "list":
+    # Display available commands
     print("="*20)
     print("Available commands:")
     print("="*20)
     for alias in commands:
         command = commands[alias]
         print(alias, command["name"], sep="\t\t")
+elif cli_args.action == "run":
+    # Execute the specified command
+    command_name = cli_args.command
+    if command_name not in commands:
+        raise ValueError(f"Unknown command: {command_name}")
+
+    command = commands[command_name]
+    cmd_args = command.get_arguments()
+    arg_values = []
+    for cmd_arg in cmd_args:
+        if cmd_arg.name in cli_args:
+            arg_values.append(cli_args.__getattribute__(cmd_arg.name))
+    subprocess.run([command.program, command.source] + arg_values)
--- src/get_commands.py
+++ src/get_commands.py
@@ -1,5 +1,6 @@
+from dataclasses import dataclass
 import os
-from typing import Mapping
+from typing import Generic, List, Mapping, Optional, TypeVar, Union
 import yaml
 
 CMD_DIR = "./commands"
@@ -9,17 +10,51 @@ ext_map = {
     "python": "py"
 }
 
+T = TypeVar("T")
 
+
+@dataclass
+class CommandArgument:
+    """
+    CommandArgument represents a single argument expected by a command.
+    This includes the name of the argument, its type, and whether or not it is required
+    """
+
+    name: str
+    description: Optional[str]
+    required: bool
+    alias: str
+
+
+@dataclass
 class Command:
     """
     Command wraps an executable script and its associated metadata
     """
 
-    def __init__(self, name: str, description: str, program: str, script: str):
-        self.name = name
-        self.description = description
-        self.program = program
-        self.script = script
+    name: str
+    description: str
+    program: str  # Executable to run the script
+    source: str  # Path to source script
+    examples: List[str]
+    _args: Optional[Mapping[str, CommandArgument]] = None
+
+    def add_argument(self, argument: CommandArgument):
+        """
+        Add an argument to this command
+        """
+        if self._args is None:
+            self._args = {}
+        name = argument.name
+        self._args[name] = argument
+
+    def get_arguments(self) -> List[CommandArgument]:
+        """
+        Returns a list of arguments to be used with this command
+        """
+        if self._args is None:
+            self._args = {}
+        return sorted(self._args.values(), key=lambda command: command.name)
 
 
 def get_commands() -> Mapping[str, Command]:
@@ -30,15 +65,31 @@ def get_commands() -> Mapping[str, Command]:
     for fn in os.listdir(CMD_DIR):
         base_name = os.path.splitext(fn)[0]
         with open(os.path.join(CMD_DIR, fn), "r") as file:
-            command = yaml.load(file.read(), Loader=yaml.CLoader)
-            ext = ext_map[command["program"]]
-            matches = command["matches"]
+            command_yaml = yaml.load(file.read(), Loader=yaml.CLoader)
+            ext = ext_map[command_yaml["program"]]
+            matches = command_yaml["matches"]
 
+            # Initialize the command
+            command = Command(
+                name=command_yaml["name"],
+                description=command_yaml["description"],
+                program=command_yaml["program"],
+                source=f"{base_name}.{ext}",
+                examples=command_yaml["examples"]
+            )
+
+            # Populate the arguments
+            for arg_name in command_yaml["args"]:
+                arg_yaml = command_yaml["args"][arg_name]
+                argument = CommandArgument(
+                    name=arg_name,
+                    description=arg_yaml["description"],
+                    alias=arg_yaml["alias"],
+                    required=arg_yaml["required"]
+                )
+                command.add_argument(argument)
+
+            # Populate all matches in the command map
             for match in matches:
-                commands[match] = {
-                    "name": command["name"],
-                    "description": command["description"],
-                    "program": command["program"],
-                    "script": f"{base_name}.{ext}",
-                }
+                commands[match] = command
     return commands
