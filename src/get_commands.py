2c53c443d75069a789d55a394cf6c387e0f3f042
Gram Liu <gram.liu226@gmail.com>
2022-10-30 03:03:51 -0400

    Fix path to commands dir

--- src/get_commands.py
+++ src/get_commands.py
@@ -3,7 +3,7 @@ import os
 from typing import Generic, List, Mapping, Optional, TypeVar, Union
 import yaml
 
-CMD_DIR = "./commands"
+CMD_DIR = os.path.realpath(os.path.dirname(__file__)) + "/../commands"
 
 ext_map = {
     "bash": "sh",
